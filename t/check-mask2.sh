6f4426cae64b3d116ad77d9ad1b5b7c8c2d830bf
Masataro Asai <guicho2.71828@gmail.com>
2015-07-03 17:16:56 +0900

    better tests

---
+++ t/check-mask2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+trap "exit 2" SIGINT
+
+run (){
+    which $1 || ( echo "$1 not in path" >&2 ; return 1 )
+
+    for i in {1..8} {10..18} {20..31}
+    do
+        echo Testing $i
+        timeout --kill-after 5 -s $i 5 $@ $i 2>>err | grep Success
+    done
+}
+
+run sbcl --disable-debugger --load check-mask2.lisp
+run ccl --batch --load check-mask2.lisp --

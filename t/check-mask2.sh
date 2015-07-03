cb16ca179c26ded478db7ca75635afc7f31ad816
Masataro Asai <guicho2.71828@gmail.com>
2015-07-03 17:23:28 +0900

    test SIGRTMIN-SIGRTMAX

--- t/check-mask2.sh
+++ t/check-mask2.sh
@@ -5,7 +5,7 @@ trap "exit 2" SIGINT
 run (){
     which $1 || ( echo "$1 not in path" >&2 ; return 1 )
 
-    for i in {1..8} {10..18} {20..31}
+    for i in {1..8} {10..18} {20..64}
     do
         echo Testing $i
         timeout --kill-after 5 -s $i 5 $@ $i 2>>err | grep Success

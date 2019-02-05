90123098b9273d1aaf954af6fc0aea51fd1b2e22
Masataro Asai <guicho2.71828@gmail.com>
2019-02-06 01:34:13 +0900

    remove sigrtmin-14 -- sigrtmin+15 --- issue #11

--- src/signals.lisp
+++ src/signals.lisp
@@ -36,13 +36,15 @@
                   +SIGEMT+ +SIGTKFLT+ +SIGIO+ +SIGCLD+ +SIGPWR+ +SIGINFO+ +SIGLOST+
                   +SIGWINCH+ +SIGUNUSED+
                   ;; realtime
-                  +SIGRTMIN+ +SIGRTMIN+1+ +SIGRTMIN+2+ +SIGRTMIN+3+
-                  +SIGRTMIN+4+ +SIGRTMIN+5+ +SIGRTMIN+6+ +SIGRTMIN+7+ +SIGRTMIN+8+
-                  +SIGRTMIN+9+ +SIGRTMIN+10+ +SIGRTMIN+11+ +SIGRTMIN+12+ +SIGRTMIN+13+
-                  +SIGRTMIN+14+ +SIGRTMIN+15+ +SIGRTMAX-14+ +SIGRTMAX-13+ +SIGRTMAX-12+
-                  +SIGRTMAX-11+ +SIGRTMAX-10+ +SIGRTMAX-9+ +SIGRTMAX-8+ +SIGRTMAX-7+
-                  +SIGRTMAX-6+ +SIGRTMAX-5+ +SIGRTMAX-4+ +SIGRTMAX-3+ +SIGRTMAX-2+
-                  +SIGRTMAX-1+ +SIGRTMAX+
+                  +SIGRTMIN+
+                  ;; +SIGRTMIN+1+ +SIGRTMIN+2+ +SIGRTMIN+3+
+                  ;; +SIGRTMIN+4+ +SIGRTMIN+5+ +SIGRTMIN+6+ +SIGRTMIN+7+ +SIGRTMIN+8+
+                  ;; +SIGRTMIN+9+ +SIGRTMIN+10+ +SIGRTMIN+11+ +SIGRTMIN+12+ +SIGRTMIN+13+
+                  ;; +SIGRTMIN+14+ +SIGRTMIN+15+ +SIGRTMAX-14+ +SIGRTMAX-13+ +SIGRTMAX-12+
+                  ;; +SIGRTMAX-11+ +SIGRTMAX-10+ +SIGRTMAX-9+ +SIGRTMAX-8+ +SIGRTMAX-7+
+                  ;; +SIGRTMAX-6+ +SIGRTMAX-5+ +SIGRTMAX-4+ +SIGRTMAX-3+ +SIGRTMAX-2+
+                  ;; +SIGRTMAX-1+
+                  +SIGRTMAX+
                   ;; bsd
                   +SIGTHR+ +SIGLIBRT+))
   (handler-case
